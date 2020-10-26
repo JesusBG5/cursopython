b0717266d33a74c9c8903100b45eb14df24fa687
Jesus <jesusjppr@gmail.com>
2020-10-26 15:11:57 -0600

    Spinbox

---
+++ tutorial25.py
@@ -0,0 +1,27 @@
+#Spinbox
+from tkinter import *
+
+def cambiar():
+	ventana.configure(background=color.get())
+
+ventana = Tk()
+ventana.geometry("400x400")
+ventana.title("Spinbox")
+color = StringVar()
+
+etiqueta1 = Label(ventana,text="Ejemplo 1")
+etiqueta1.place(x=20,y=50)
+
+lista = Spinbox(ventana,textvariable=color,values=("Blue","Yellow","Red","Green"))
+lista.place(x=120,y=50)
+
+etiqueta2 = Label(ventana,text="Ejemplo 2")
+etiqueta2.place(x=20,y=150)
+
+lista2 = Spinbox(ventana,from_=1900,to=2020)
+lista2.place(x=120,y=150)
+
+boton = Button(ventana,text="Cambiar",command=cambiar)
+boton.place(x=50,y=200)
+
+ventana.mainloop()
